577d64356a0140083f7180686d638d51dcc83445
lukasbuse4 <lukasbuse4@gmail.com>
2022-06-28 07:54:59 +0300

    Initial Commit

---
+++ Shared/Bday_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bday_appApp.swift
+//  Shared
+//
+//  Created by Josh Buse on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Bday_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
